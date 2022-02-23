a788cc3f77a96b879af4e5209e99656de7ac3c28
Jakob Heinemann <jakob@jakobheinemann.de>
2022-02-23 18:52:13 +0100

    version increment

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022021600;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022022300;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700;        // Requires this Moodle version.
 $plugin->component = 'profilefield_verydynamicmenu'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
